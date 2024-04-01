403852d3b75d83a45c594241a4f5d5a3db78a2de
farzan <farzanullah07@gmail.com>
2024-04-02 01:55:46 +0530

    frontend draft v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
